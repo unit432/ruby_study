120eae88794a7363f3d68ededba8557677cb66c7
Chen <cdong@etfflows.com>
2022-02-22 22:22:05 -0700

    add more test

--- spec/merge_sort_spec.rb
+++ spec/merge_sort_spec.rb
@@ -6,5 +6,11 @@ describe MergeSort do
       expect(MergeSort.new.sort([88, 76, 3, 4, 2, 11, 9, 12, 1, 8, 888]))
         .to eq([1, 2, 3, 4, 8, 9, 11, 12, 76, 88, 888])
     end
+
+    context 'when array is empty' do
+      it 'returns empty array' do
+        expect(MergeSort.new.sort([])).to eq([])
+      end
+    end
   end
 end
